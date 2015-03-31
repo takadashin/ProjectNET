117be6f8b8e393916cea4a0e5c45de71ecf2ae2f
linhngoc184 <linhngoc1841@yahoo.com>
2015-03-31 13:07:03 -0400

    Linh 31/3/2015 Account form

---
+++ Pharmacy/FrmAccounts.Designer.cs
@@ -0,0 +1,420 @@
+﻿namespace Pharmacy
+{
+    partial class FrmAccounts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbGender = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtInsurance = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAdd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvAccounts = new System.Windows.Forms.DataGridView();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.cmbType = new System.Windows.Forms.ComboBox();
+            this.dtpDateOfBirth = new System.Windows.Forms.DateTimePicker();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtReTypePass = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAccounts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbGender
+            // 
+            this.cmbGender.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbGender.FormattingEnabled = true;
+            this.cmbGender.Location = new System.Drawing.Point(86, 408);
+            this.cmbGender.MaxLength = 10;
+            this.cmbGender.Name = "cmbGender";
+            this.cmbGender.Size = new System.Drawing.Size(331, 21);
+            this.cmbGender.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(20, 410);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 13);
+            this.label8.TabIndex = 43;
+            this.label8.Text = "Gender";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(509, 526);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(456, 347);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 42;
+            this.label7.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(533, 344);
+            this.txtName.MaxLength = 50;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(375, 20);
+            this.txtName.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(590, 526);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 14;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(21, 468);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 41;
+            this.label6.Text = "Insurance";
+            // 
+            // txtInsurance
+            // 
+            this.txtInsurance.Location = new System.Drawing.Point(86, 465);
+            this.txtInsurance.MaxLength = 50;
+            this.txtInsurance.Name = "txtInsurance";
+            this.txtInsurance.Size = new System.Drawing.Size(331, 20);
+            this.txtInsurance.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 438);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 38;
+            this.label4.Text = "Address";
+            // 
+            // txtAdd
+            // 
+            this.txtAdd.Location = new System.Drawing.Point(86, 435);
+            this.txtAdd.MaxLength = 50;
+            this.txtAdd.Name = "txtAdd";
+            this.txtAdd.Size = new System.Drawing.Size(331, 20);
+            this.txtAdd.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(454, 406);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 35;
+            this.label3.Text = "Date of birth";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 351);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "ID";
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(86, 348);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(331, 20);
+            this.txtId.TabIndex = 2;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(833, 527);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 17;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(752, 527);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 16;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(671, 527);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 15;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(833, 10);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "List of Accounts";
+            // 
+            // dgvAccounts
+            // 
+            this.dgvAccounts.AllowUserToAddRows = false;
+            this.dgvAccounts.AllowUserToDeleteRows = false;
+            this.dgvAccounts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAccounts.Location = new System.Drawing.Point(23, 66);
+            this.dgvAccounts.Name = "dgvAccounts";
+            this.dgvAccounts.ReadOnly = true;
+            this.dgvAccounts.Size = new System.Drawing.Size(885, 262);
+            this.dgvAccounts.TabIndex = 24;
+            this.dgvAccounts.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvAccounts_CellClick);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(23, 13);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(804, 20);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(456, 376);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 13);
+            this.label5.TabIndex = 48;
+            this.label5.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(533, 373);
+            this.txtPassword.MaxLength = 50;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(132, 20);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(21, 380);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 13);
+            this.label9.TabIndex = 47;
+            this.label9.Text = "User Name";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(86, 377);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(331, 20);
+            this.txtUserName.TabIndex = 4;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(454, 441);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(78, 13);
+            this.label10.TabIndex = 50;
+            this.label10.Text = "Phone Number";
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.Location = new System.Drawing.Point(533, 435);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(373, 20);
+            this.txtPhoneNumber.TabIndex = 10;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(454, 468);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(31, 13);
+            this.label11.TabIndex = 51;
+            this.label11.Text = "Type";
+            // 
+            // cmbType
+            // 
+            this.cmbType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbType.FormattingEnabled = true;
+            this.cmbType.Location = new System.Drawing.Point(533, 465);
+            this.cmbType.MaxLength = 10;
+            this.cmbType.Name = "cmbType";
+            this.cmbType.Size = new System.Drawing.Size(373, 21);
+            this.cmbType.TabIndex = 12;
+            // 
+            // dtpDateOfBirth
+            // 
+            this.dtpDateOfBirth.Location = new System.Drawing.Point(533, 402);
+            this.dtpDateOfBirth.Name = "dtpDateOfBirth";
+            this.dtpDateOfBirth.Size = new System.Drawing.Size(373, 20);
+            this.dtpDateOfBirth.TabIndex = 8;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(670, 376);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(90, 13);
+            this.label12.TabIndex = 55;
+            this.label12.Text = "Retype Password";
+            // 
+            // txtReTypePass
+            // 
+            this.txtReTypePass.Location = new System.Drawing.Point(763, 373);
+            this.txtReTypePass.MaxLength = 50;
+            this.txtReTypePass.Name = "txtReTypePass";
+            this.txtReTypePass.Size = new System.Drawing.Size(143, 20);
+            this.txtReTypePass.TabIndex = 6;
+            // 
+            // FrmAccounts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 558);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.txtReTypePass);
+            this.Controls.Add(this.dtpDateOfBirth);
+            this.Controls.Add(this.cmbType);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.cmbGender);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtInsurance);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAdd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvAccounts);
+            this.Controls.Add(this.txtSearch);
+            this.Name = "FrmAccounts";
+            this.Text = "FrmAccounts";
+            this.Load += new System.EventHandler(this.FrmAccounts_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAccounts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbGender;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtInsurance;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAdd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvAccounts;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox cmbType;
+        private System.Windows.Forms.DateTimePicker dtpDateOfBirth;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtReTypePass;
+    }
+}
---
+++ Pharmacy/FrmAccounts.cs
@@ -0,0 +1,196 @@
+﻿using Pharmacy.Domain;
+using Pharmacy.Service;
+using Pharmacy.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pharmacy
+{
+    public partial class FrmAccounts : Form
+    {
+        AccountService accService = new AccountService();
+        Accounts account = new Accounts();
+
+        private void setAccount(Accounts account, bool insert)
+        {
+            account.Name = txtName.Text;
+            account.UserName = txtUserName.Text;
+            account.Password = txtPassword.Text;
+            account.DateOfBirth = dtpDateOfBirth.Value;
+            account.Gender = cmbGender.Text;
+            account.Address = txtAdd.Text;
+            account.PhoneNum = txtPhoneNumber.Text;
+            account.Insurance = txtInsurance.Text;
+            account.Type = cmbType.Text;            
+        }
+
+        
+        private bool validate(bool add)
+        {
+            if (Utils.CommonMethod.checkEmptyString(txtName.Text, "Please Enter Name", "Error"))
+            {
+                return false;
+            }
+
+            if (Utils.CommonMethod.checkEmptyString(txtUserName.Text, "Please Enter User Name", "Error"))
+            {
+                return false;
+            }
+            
+            if (add && accService.getByUserName(txtUserName.Text) != null)
+            {
+                    MessageBox.Show("UserName was exist already, please enter again", "Error");
+                    return false;
+            }
+
+            if (Utils.CommonMethod.checkEmptyString(txtPassword.Text, "Please Enter Password", "Error"))
+            {
+                return false;
+            }
+
+            if (add || (!add && txtPassword.Text.Trim() != account.Password.Trim()))
+            {
+
+                if (Utils.CommonMethod.checkEmptyString(txtReTypePass.Text, "Please Enter ReType Password", "Error"))
+                {
+                    return false;
+                }
+
+                if (txtPassword.Text != txtReTypePass.Text)
+                {
+                    MessageBox.Show("Password is different from ReType Password, please enter again", "Error");
+                    return false;
+                }
+            }
+           
+            return true;
+        }
+
+        public FrmAccounts()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FrmAccounts_Load(object sender, EventArgs e)
+        {
+            DataSet ds = accService.dsGetAll(Constants.ID, true);
+            dgvAccounts.DataSource = ds.Tables[0];
+            cmbGender.DataSource = Constants.gender;
+            cmbGender.SelectedIndex = 0;
+            cmbType.DataSource = Constants.type;
+            cmbType.SelectedIndex = 0;
+            btnDelete.Enabled = false;
+            btnSave.Enabled = false;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            foreach (Control control in this.Controls)
+            {
+                if (control.Name.Substring(0, 3) == "txt")
+                    control.Text = "";
+            }
+            cmbGender.SelectedIndex = 0;
+            cmbType.SelectedIndex = 0;
+            DataSet ds = accService.dsGetAll(Constants.ID, true);
+            dgvAccounts.DataSource = ds.Tables[0];
+            btnAdd.Enabled = true;
+            btnSave.Enabled = false;
+            btnDelete.Enabled = false;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (validate(true))
+            {
+                Accounts accountNew = new Accounts();
+                setAccount(accountNew, true);
+                if (accService.insertObject(accountNew))
+                {
+                    DataSet ds = accService.dsGetAll(Constants.ID, true);
+                    dgvAccounts.DataSource = ds.Tables[0];
+                    btnClear_Click(sender, e);
+                }
+            }     
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (validate(false))
+            {
+                setAccount(account, false);
+                if (accService.updatedObject(account))
+                {
+                    DataSet ds = accService.dsGetAll(Constants.ID, true);
+                    dgvAccounts.DataSource = ds.Tables[0];
+                }
+            }    
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (accService.deleteById(account.Id))
+            {
+                DataSet ds = accService.dsGetAll(Constants.ID, true);
+                dgvAccounts.DataSource = ds.Tables[0];
+                btnClear_Click(sender, e);
+            }
+        }
+
+        private void dgvAccounts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex != -1)
+            {
+                account = accService.getById(dgvAccounts.Rows[e.RowIndex].Cells[0].Value);
+                txtId.Text = account.Id.ToString();
+                txtName.Text = account.Name;
+                txtUserName.Text = account.UserName;
+                dtpDateOfBirth.Value = account.DateOfBirth;
+                txtPassword.Text = account.Password;
+                cmbGender.SelectedIndex = Utils.CommonMethod.getComboboxIndex(Constants.gender, account.Gender); 
+                txtAdd.Text = account.Address;
+                txtPhoneNumber.Text = account.PhoneNum;
+                txtInsurance.Text = account.Insurance;
+                cmbType.SelectedIndex = Utils.CommonMethod.getComboboxIndex(Constants.type, account.Type);
+                btnAdd.Enabled = false;
+                btnDelete.Enabled = true;
+                btnSave.Enabled = true;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            List<Criterion> criterions = new List<Criterion>();
+            if (!Utils.CommonMethod.checkNotInt(txtSearch.Text,null,null))
+            {
+                criterions.Add(new Criterion(Constants.ID, txtSearch.Text, "or"));
+            }
+            if(!Utils.CommonMethod.checkNotDateTime(txtSearch.Text,null,null))
+            {
+                 criterions.Add(new Criterion(Constants.ACC_BIRTH, txtSearch.Text, "or"));
+            }
+            criterions.Add(new Criterion(Constants.ACC_NAME, txtSearch.Text, "or"));
+            criterions.Add(new Criterion(Constants.ACC_USERNAME, txtSearch.Text, "or"));
+            criterions.Add(new Criterion(Constants.ACC_PASSWORD, txtSearch.Text, "or"));
+            criterions.Add(new Criterion(Constants.ACC_GENDER, txtSearch.Text, "or"));
+            criterions.Add(new Criterion(Constants.ACC_ADD, txtSearch.Text, "or"));
+            criterions.Add(new Criterion(Constants.ACC_PHONE, txtSearch.Text, "or"));
+            criterions.Add(new Criterion(Constants.ACC_INS, txtSearch.Text, "or"));
+            criterions.Add(new Criterion(Constants.ACC_TYPE, txtSearch.Text));
+            DataSet ds = accService.dsGetByCriterion(criterions, Constants.ID, true);
+            dgvAccounts.DataSource = ds.Tables[0];
+        }
+    }
+}
